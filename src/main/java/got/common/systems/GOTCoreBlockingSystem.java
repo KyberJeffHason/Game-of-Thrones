1acd9f46b8666460bdbc2b3553bcdc4111a43abb
JeffHason <67012500+KyberJeffHason@users.noreply.github.com>
2024-10-17 20:10:01 +0300

    Прописал углы блоков для оружий

--- src/main/java/got/common/systems/GOTCoreBlockingSystem.java
+++ src/main/java/got/common/systems/GOTCoreBlockingSystem.java
@@ -1,6 +1,8 @@
 package got.common.systems;
 
-import got.common.item.weapon.GOTItemBattleaxe;
+import got.common.item.tool.GOTItemAxe;
+import got.common.item.weapon.*;
+import net.minecraft.item.ItemSword;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -13,10 +15,16 @@ public class GOTCoreBlockingSystem {
     }
 
     public static void registerWeapons() {
-        registerWeapon(GOTItemBattleaxe.class, 10.0f, 90.0f);
+        registerWeapon(GOTItemSword.class, 35.0f, 35.0f);
+        registerWeapon(ItemSword.class, 35.0f, 35.0f);
+        registerWeapon(GOTItemHammer.class, 20.0f, 20.0f);
+        registerWeapon(GOTItemPolearm.class, 25.0f, 25.0f);
+        registerWeapon(GOTItemAxe.class, 28.0f, 28.0f);
+        registerWeapon(GOTItemDagger.class, 20.0f, 20.0f);
+        registerWeapon(GOTItemDornePolearm.class, 10.0f, 90.0f);
     }
 
     public static float[] getBlockAngles(Class<?> weaponClass) {
-        return weaponBlockAngles.getOrDefault(weaponClass, new float[]{45.0f, 45.0f});
+        return weaponBlockAngles.getOrDefault(weaponClass, new float[]{35.0f, 35.0f});
     }
 }
